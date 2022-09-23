e7a542868befdca50b34839244c547c9ec185fc1
FinoanaRandria <niriantsoa.randriambololomanana@esti.mg>
2022-09-23 19:29:20 +0300

    Dom

--- src/components/Cars.js
+++ src/components/Cars.js
@@ -4,23 +4,25 @@ const Car = (props) => {
    /* props premet de recuperer les composants de lasa objects */
 
    const CondColor = props.Color ? (<p >Couleur:{props.Color}</p>) : (<p>Couleur:Aucun</p>)
-
+   const style = {
+      wrapper : { 
+         backgroundColor: "#00FFFF"
+         , width: '400px',
+          margin: "5px auto", 
+          padding: "10px" }
+   }
    if (props.children != null) {
-      return (/* jsx a besoin d'un elment parent izay mahatonga any le div eo */
-         <div style={{ backgroundColor: "	#00FFFF", width: '400px', margin: "5px auto", padding: "10px" }}>
+      return (/* jsx a besoin d'un element parent izay mahatonga any le div eo */
+         <div style={style.wrapper}>
             <p>Marque:{props.children}</p>
             {CondColor}
             {/* {}permet d'introduire du code js dans du jsx */}
+            <p>Date de sortie:{props.date}</p>
          </div>
-
+            
       )
    } else {
-      return (
-         <div style={{ backgroundColor: "red", width: '400px', margin: "5px auto", padding: "10px" }}>
-            <p>Aucun data</p>
-         </div>
-
-
+      return ( null
       )
    }/* fonction miretoune zavtr faona */
 
--- src/components/Mycar.js
+++ src/components/Mycar.js
@@ -1,12 +1,12 @@
-import React, { Component } from "react";
+import React, { Children, Component } from "react";
 import Car from "./Cars";
 import Footer from "./Footer";
 class Mycar extends Component {
   noCopy = () => {
     alert("tu es entrain de me copier");
   };
-  
-              /* addStyle = (e) => {
+
+  /* addStyle = (e) => {
     console.log(e.target);
     if (e.target.classList.contains("styed")) {
       e.target.classList.remove("styed");
@@ -14,23 +14,49 @@ class Mycar extends Component {
       e.target.classList.add("styed");
     }
   }; */
+     ajout10 = ()=>{
+      const update =this.state.voiture.map((pram)=>{
+         return pram.date +10
+      })/* la nouveau state */
+     }
+
+  state = {
+    voiture: [
+      { nom: "renault", color: "skyblue", date: 2023 },
+      { nom: "nissan", color: "pink", date: 2022},
+      { nom: "toyota", color: "white", date: 2024},
+    ],
+  };
   render() {
-    console.log(this);
     return (
       <div>
         <h1>{this.props.title}</h1>
         {/* onMouseOver={this.addStyle} */}
         <p onCopy={this.noCopy}>
-          saluttttttttttttttttttttttttttt ne me copie pas{" "}
+          saluttttttttttttttttttttttttttt ne me copie pas
         </p>
         {/* props permet de recuperer les element dans un parents sous forme  objects */}
-        <Car Color="red">renault</Car>
+        <Car
+          Color={this.state.voiture[0].color}
+          date={this.state.voiture[0].date}
+        >
+          {this.state.voiture[0].nom}
+        </Car>
         {/* peuvent aussi prendre des parametres ex color eto */}
         {/* peuvent encore contenir des elements enfants */}
-        <Car Color="blue"></Car>
-        <Car Color="">ford</Car>
-
-         
+        <Car
+          Color={this.state.voiture[1].color}
+          date={this.state.voiture[1].date}
+        >
+          {this.state.voiture[1].nom}
+        </Car>
+        <Car
+          Color={this.state.voiture[2].color}
+          date={this.state.voiture[2].date}
+        >
+          {this.state.voiture[2].nom}
+        </Car>
+        <button  onClick={this.ajout10}>+10</button>
       </div>
     );
   }
